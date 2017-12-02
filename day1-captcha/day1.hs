45ec47a10ce59ed2c44b839dfe74ad1b30f459ea
oscar hansson <racso.hansson@gmail.com>
2017-12-02 12:42:02 +0100

    fixed error in day1

--- day1-captcha/day1.hs
+++ day1-captcha/day1.hs
@@ -3,16 +3,11 @@ import Data.Char
 stringToIntegerList :: String -> [Int]
 stringToIntegerList string = [digitToInt element | element <- string]
 
-makeList :: IO ([Int])
-makeList = do
-      content <- readFile "Input.txt"
+match :: FilePath -> IO Int
+match path = do
+      content <- readFile path
       let a = [digitToInt element | element <- content]
-      let b = match a
-      return a
+      return (sumMatch a)
 
-match :: IO (Int) -> IO Int
-match list = sum [if (i == (length list)-1) then (if (last list) == (head list) then (head list) else 0) else (if(list !! (i+1) == element) then element else 0) | (i,element) <- zip [0..] list]
-
-
---main :: IO ()
---main =  c = makeList "input.txt"
+sumMatch :: [Int] -> Int
+sumMatch list = sum [if (i == (length list)-1) then (if (last list) == (head list) then (head list) else 0) else (if(list !! (i+1) == element) then element else 0) | (i,element) <- zip [0..] list]
