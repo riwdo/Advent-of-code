01f44cc491b1e082dff0552206a80a4682e0c955
oscar hansson <racso.hansson@gmail.com>
2017-12-04 19:34:43 +0100

    day2-b

--- day2-checksum/day2.hs
+++ day2-checksum/day2.hs
@@ -10,3 +10,18 @@ parseMatrix s =  (map (map read . words) $ lines s)
 
 difference :: [Int] -> Int
 difference list = maximum list - minimum list
+
+
+--B
+
+readInputEven :: FilePath -> IO Int
+readInputEven path = do
+      content <- readFile path
+      let matrix = parseMatrix content :: [[Int]]
+      let sumation = sum [addDivEven list | list <- matrix]
+      return sumation
+
+addDivEven :: [Int] -> Int
+addDivEven [] = 0
+addDivEven (x:list) = sum [checkMod x element | element <- (list)] + addDivEven list
+                                    where checkMod n1 n2 = if(n1 `mod` n2 == 0) then n1 `div` n2 else (if(n2 `mod` n1 == 0) then n2 `div` n1 else 0)
