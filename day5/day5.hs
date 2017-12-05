56116056e6d7e51fe816bf60ab33de1a4095a3b1
oscar hansson <racso.hansson@gmail.com>
2017-12-05 11:37:02 +0100

    Fixed small error

--- day5/day5.hs
+++ day5/day5.hs
@@ -2,11 +2,13 @@ readInput :: FilePath -> IO Int
 readInput path = do
       content <- readFile path
       let list = parseStringArray (lines content)
-      return (findExit list 0 0 (length list) (head list))
+      --return list
+      return (findExit list 0 0 (head list))
 
 parseStringArray :: [String] -> [Int]
 parseStringArray list = [read element :: Int | element <- list]
 
-findExit :: [Int] -> Int -> Int -> Int -> Int -> Int
-findExit list i steps l stepSize | (i+stepSize) < 0  || (i+stepSize) >= (l) = steps
-                                 | otherwise = findExit (take i list ++ [stepSize+1] ++ drop (i+1) list) (i+stepSize) (steps+1) l (list !! (i+stepSize))
+findExit :: [Int] -> Int -> Int -> Int -> Int
+findExit list i steps stepSize | (i) < (length list) && (i) >= (-1) = findExit updatedList (newIndex) (steps+1) (updatedList !! (newIndex))
+                               | otherwise = steps
+                               where (updatedList,newIndex) = (take i list ++ [stepSize+1] ++ drop (i+1) list, (i+stepSize))
